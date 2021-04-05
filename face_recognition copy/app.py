5519f16a5ea2bd95f05fee526e09492d0b07062c
shreyanshsatvik <shreyanshsatvik@gmail.com>
2021-04-05 10:37:55 +0530

    Initial Change

--- face_recognition copy/app.py
+++ face_recognition copy/app.py
@@ -130,5 +130,6 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application  
     # on the local development server.
     # check commentg 
+    #check
     app.run()
     
